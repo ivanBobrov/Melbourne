48a1ba62927aa11a7cfba678fa4dfbe4291d6d9d
Bobrov Ivan <id.bobr@gmail.com>
2018-05-19 12:01:01 +0300

    mail-handler.php refactoring

--- page-mail-handler.php
+++ page-mail-handler.php
@@ -4,27 +4,31 @@ if (!empty($_POST["mail"]) && !empty($_POST["file"])) {
 	$pMail = $_POST["mail"];
 	$pFile = $_POST["file"];
 	
+	saveMail($pMail);
+	sendFileToUser($pFile);
+}
+
+function saveMail($mail) {
 	$dir = wp_upload_dir()['basedir'];
-	saveMail($pMail, "$dir/mail.list");
+	$file = "$dir/mail.list";
+	file_put_contents($file, $mail.PHP_EOL , FILE_APPEND | LOCK_EX);
+}
 
-	
-	$file = "$dir/$pFile";
+function sendFileToUser($file) {
+	$dir = wp_upload_dir()['basedir'];
+	$path = "$dir/$file";
 
-	$filename = 'ASKKM_fest_2018.pdf';
+	$renderedFileName = 'ASKKM_fest_2018.pdf';
 	header("Pragma: public");
 	header("Expires: 0");
 	header("Cache-Control: must-revalidate, post-check=0, pre-check=0");
 	header("Cache-Control: public");
 	header("Content-Description: File Transfer");
 	header("Content-Type: application/pdf");
-	header("Content-Disposition: attachment; filename=$filename");
+	header("Content-Disposition: attachment; filename=$renderedFileName");
 	header("Content-Transfer-Encoding: binary");
 
-	readfile($file);
-}
-
-function saveMail($mail, $filename) {
-	file_put_contents($filename, $mail.PHP_EOL , FILE_APPEND | LOCK_EX);
+	readfile($path);
 }
 
 ?>
