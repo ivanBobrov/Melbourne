86f9814080bc61c3bff512970262bd140e7401f6
Bobrov Ivan <id.bobr@gmail.com>
2018-05-24 00:21:00 +0300

    mail.list deleting file on update

--- inc/mail-handler.php
+++ inc/mail-handler.php
@@ -3,7 +3,7 @@
 	Module to support saving mails through page-mail-handler.php
 */
 
-function melbourne_mail_handler_setup() {
+function melbourne_add_mail_table() {
 	global $wpdb;
 	$table_name = $wpdb->prefix . 'melbourne_mail';
 	if ($wpdb->get_var("SHOW TABLES LIKE '$table_name'") != $table_name) {
@@ -20,7 +20,45 @@ function melbourne_mail_handler_setup() {
 		dbDelta($sql);
 	}
 }
-//add_action('upgrader_process_complete', 'melbourne_mail_handler_setup')
-melbourne_mail_handler_setup();
+
+function melbourne_save_mail($mail) {
+	global $wpdb;
+
+	$mail_id = $wpdb->get_var($wpdb->prepare("SELECT id FROM " . $wpdb->prefix . 
+											 "melbourne_mail WHERE mail = %s LIMIT 1", $mail));
+
+	if ($mail_id == NULL) {
+		$date = current_time('mysql');
+		$wpdb->insert($wpdb->prefix . 'melbourne_mail', array(
+			'mail' => $mail,
+			'insert_date' => $date
+		));
+	}
+}
+
+function melbourne_move_mail_to_db() {
+	global $wpdb;
+
+	$dir = wp_upload_dir()['basedir'];
+	$file = "$dir/mail.list";
+
+	if (file_exists($file)) {
+		$handle = fopen($file, 'r');
+		if ($handle) {
+			while (($line = fgets($handle)) !== false) {
+				melbourne_save_mail(trim($line));
+			}
+		}
+
+		fclose($handle);
+		unlink($file);
+	}
+}
+
+function melbourne_mail_handler_setup() {
+	melbourne_add_mail_table();
+	melbourne_move_mail_to_db();
+}
+add_action('upgrader_process_complete', 'melbourne_mail_handler_setup')
 
 ?>
